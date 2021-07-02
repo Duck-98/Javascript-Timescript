575bda4de627d1316499e5cf304b027dbc089cb0
duck <godqhr2256@gmail.com>
2021-07-02 22:03:00 +0900

    type -> interface 전환

--- Typescript/app.ts
+++ Typescript/app.ts
@@ -1,28 +1,32 @@
-
-type Store = {
+interface Store  {
   currentPage : number;
-  feeds : NewsFeed[]; //ㅜ
+  feeds : NewsFeed[];
 } // Store의 타입 설정
-type News = {
-  id : number;
-  title: string;
-  time_ago : string;
-  url : string;
-  user : string;
-  content : string;
+
+interface News  {
+  readonly id : number;
+  readonly title: string;
+  readonly time_ago : string;
+  readonly url : string;
+  readonly user : string;
+  readonly content : string;
 }
-type NewsFeed = News & {
-  comments_count : number;
-  points : string;
+
+interface NewsFeed  extends News {
+  readonly comments_count : number;
+  readonly points : string;
   read?: boolean; //선택속성(있을 때도 있고 없을 때도 있음)
 } // NewsFeed 타입 설정.
-type NewsDetail =  News & {
-  comments : NewsComment[];
+
+interface NewsDetail  extends News  {
+  readonly comments : NewsComment[];
 }
-type NewsComment = News & {
-  comments : NewsComment[];
-  level : number;
+
+interface NewsComment extends News  {
+  readonly comments : NewsComment[];
+  readonly level : number;
 }
+
 const container: HTMLElement | null = document.getElementById('root'); //dom element를 받아옴
 const ajax: XMLHttpRequest = new XMLHttpRequest(); //객체 타이핑
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
