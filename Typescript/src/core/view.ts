1c9868d07489ca6df468537dd4539d5b999b4bc1
duck <godqhr2256@gmail.com>
2021-07-08 22:46:40 +0900

    디렉터리 구조 생성 및 분리, 캡슐화 완료

---
+++ Typescript/src/core/view.ts
@@ -0,0 +1,45 @@
+export default abstract class View {
+    private template: string;
+    private renderTemplate: string;
+    private container: HTMLElement;
+    private htmlList: string[];
+  
+    constructor(containerId: string, template: string) {
+      const containerElement = document.getElementById(containerId);
+  
+      if (!containerElement) {
+        throw '최상위 컨테이너가 없어 UI를 진행하지 못합니다.';
+      }
+  
+      this.container = containerElement;
+      this.template = template;
+      this.renderTemplate = template;
+      this.htmlList = [];
+    }
+  
+    protected updateView(): void {
+      this.container.innerHTML = this.renderTemplate;
+      this.renderTemplate = this.template; // 초기 값으로 변환
+    }
+    
+    protected addHtml(htmlString: string): void {
+      this.htmlList.push(htmlString);
+    }
+  
+    protected getHtml(): string {
+      const snapshot = this.htmlList.join('');
+      this.clearHtmlList();// 초기화
+      return snapshot;
+    }
+  
+    protected setTemplateData(key: string, value: string): void {
+      this.renderTemplate = this.renderTemplate.replace(`{{__${key}__}}`, value);
+    }
+  
+    private clearHtmlList(): void {
+      this.htmlList = [];
+    }
+  
+    abstract render(): void; // 추상메소드 , 자식 메소드에게 render()을 구현하게 하기 위해 사용
+  }
+  
