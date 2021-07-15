42941026f5053df8c922f00da52551bb88a97170
duck <godqhr2256@gmail.com>
2021-07-15 22:42:43 +0900

    6.6 식(연산 계산 개념)

---
+++ 학습 정리/6.3/app.ts
@@ -0,0 +1,8 @@
+
+
+function addAged(aged :number) :number{
+    return aged + 1;
+} 
+let aged : number = addAged(30);
+
+console.log(aged);
---
+++ 학습 정리/6.4 변수와 상수/app.js
@@ -0,0 +1,9 @@
+let x = 10;
+const y= 100;
+
+const obj = {
+    height : 200,
+    width : 300,
+};
+obj.height = 300;
+ 
