b026ff3495dd83fb54b32b901f6387f6d2471750
duck <godqhr2256@gmail.com>
2021-07-15 22:59:14 +0900

    6.6 참조와 복사 개념

---
+++ 학습 정리/6.5 식 copy/app.js
@@ -0,0 +1,23 @@
+ 
+ //const colors = {'red', 'yellow','black'};
+ //const red = colors[0];
+// const yellow = colors[1];
+const [red,yellow, black] = colors;
+
+const [, yellow] = colors;
+const {white,green} = Colors;
+
+yellow;
+
+let a = 10;
+let b = '10';
+
+if(a === b){
+a= 0;
+} else{
+    a = 1;
+}
+// 축약 버전 삼항연산자
+a= (a === b)? 0 : 1;
+ 
+
