c0ecc05db8dc29c6da0a03657402eae134f2bde9
duck <godqhr2256@gmail.com>
2021-07-24 22:21:18 +0900

    개념정리 인스턴스

--- 학습 정리/6.23 인스턴스/app.ts
+++ 학습 정리/6.23 인스턴스/app.ts
@@ -1,4 +1,6 @@
- function CartV1(){
+ //함수를 이용해서 인스턴스 객체를 만드는 방법
+ 
+ function CartV1(){ //C를 대문자로 쓴 이유 new 연산자를 써야하는 함수이기 때문에.(암묵적 룰)
      this.cart = [];
      this.currentId = 0;
  }
@@ -33,10 +35,10 @@
  shoppingCartV1.addItem(CartV1.createItem('두부', 2000));
 
  console.log(shoppingCartV1.cart);
-
+// 클래스를 이용해서 인스턴스 객체를 만드는 방법.
  class CartV2 {
      static createItem = (name, price) => ({
-         name, price
+         name, price //정적 메소드(static) -> 인스턴스 객체에는 들어나지 않고 클래스 자체에 위치
      });
      
      cart;
@@ -46,6 +48,32 @@
          this.currentId = 0;
          this.cart = [];
      }
+
+     getNewId = ()=> {
+         this.currentId++;
+         return this.currentId;
+     }
+
+     addItem = item => {
+          this.cart.push({
+              ...item,
+              id: this.getNewId(),
+          });
+     }
  
- 
+    clearCart = () =>{
+        this.currentId = 0;
+        this.cart = [];
+    }
 }
+
+const shoppingCartV2 = new CartV2();
+
+ shoppingCartV1.addItem(CartV2.createItem('수박', 8000));
+ shoppingCartV1.addItem(CartV2.createItem('사과', 12000));
+ shoppingCartV1.addItem(CartV2.createItem('두부', 2000));
+
+console.log(shoppingCartV2.cart);
+
+//결론 함수를 이용하여 인스턴스 객체를 만드는 것 보다는 클래스를 이용하여 만드는 것이
+//훨씬 직관적이며 효율적임을 알 수 있었다.
