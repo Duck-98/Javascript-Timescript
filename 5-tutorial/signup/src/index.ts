db65adb9dd108988915310d6b0527e73e9da5b08
duck <godqhr2256@gmail.com>
2021-08-15 22:37:16 +0900

    파트 5 회원가입

--- 5-tutorial/signup/src/index.ts
+++ 5-tutorial/signup/src/index.ts
@@ -1,7 +1,7 @@
 import { AnyObject } from './types';
 import App from './app';
 
-declare global {
+declare global { //글로벌 영역 
   interface Window {
     Handlebars: {
       compile: (template: string) => (data: AnyObject) => string;
@@ -10,7 +10,7 @@ declare global {
   }
 }
 
-const app = new App('#root', {
+const app = new App('#root', { // app -> class
   title: 'Javascript & TypeScript Essential Chapter 5 - Sign up'
 });
 
